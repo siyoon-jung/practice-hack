d4135ad4b300cc2c389cf761176d21e9641bcba8
siyoon-jung <siyoon.syj@gmail.com>
2024-08-20 12:09:50 +0900

    feat: style 관련 설정 및 테스트

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import GoogleMap from "./modules/views/GoogleMap";
 
+import styles from './App.module.scss';
+
 function App() {
   return (
+    <>
       <GoogleMap />
+      <div className={styles.wrap} />
+      </>
   );
 }
 
